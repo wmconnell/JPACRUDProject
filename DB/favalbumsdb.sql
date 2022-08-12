784f3ed5469180c2512873be47a3e8c72d9c6fb5
wmconnell <wyatt.connell@gmail.com>
2022-08-12 18:58:22 -0400

    stub schema and album table

---
+++ DB/favalbumsdb.sql
@@ -0,0 +1,38 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema favalbumsdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `favalbumsdb` ;
+
+-- -----------------------------------------------------
+-- Schema favalbumsdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `favalbumsdb` DEFAULT CHARACTER SET utf8 ;
+USE `favalbumsdb` ;
+
+-- -----------------------------------------------------
+-- Table `album`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `album` ;
+
+CREATE TABLE IF NOT EXISTS `album` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS albumuser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'albumuser'@'localhost' IDENTIFIED BY 'albumuser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'albumuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
